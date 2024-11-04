b5c7826f3fdb67401330e1267ae1d945e7cf9017
nataliaCHich <nschicherova@gmail.com>
2024-11-04 10:34:09 +0100

    Initial Commit

---
+++ testGitHub/testGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitHubApp.swift
+//  testGitHub
+//
+//  Created by Chicherova Natalia2 on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct testGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
