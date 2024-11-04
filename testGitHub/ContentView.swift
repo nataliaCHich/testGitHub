4820229644e874300286bf3767f0b36a84d0b4c0
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-04 10:45:32 +0100

    Add welcome message

--- testGitHub/ContentView.swift
+++ testGitHub/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Jan was here 🐈😂")
         }
         .padding()
     }
